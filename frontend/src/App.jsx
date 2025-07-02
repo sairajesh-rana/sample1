c17c554f9e39c9f95943c5da01c0ab066a3bbba2
sairajesh-rana <metterajesh@gmail.com>
2025-07-02 16:24:06 +0530

    add .jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import WishlistPage from './components/Products/WishlistPage';
 import ProductGrid from './components/Products/ProductGrid';
 import Carousel from './components/banner1/section1';
 import CartPage from './components/Products/cartModel';
-import { ProductDetail,ShopCategories } from './components/Products/Shoppage';
+import { ProductDetail,ShopCategories } from './components/Products/Shoppage.jsx';
 import Footer from './components/Footer/Footer';
 import CombinedAccountFlow from './components/Form/Contact';
 import Banners from './components/Poster/Banner';
