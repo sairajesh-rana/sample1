72ce476d4b75b4a8e369bfb035914df4b112cf19
sairajesh-rana <metterajesh@gmail.com>
2025-07-02 16:46:10 +0530

    file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import WishlistPage from './components/Products/WishlistPage';
 import ProductGrid from './components/Products/ProductGrid';
 import Carousel from './components/banner1/section1';
 import CartPage from './components/Products/cartModel';
-import Shoppage from './components/Products/Shoppage.jsx';
+import Shoppage from './components/Products/Shoppage1';
 import Footer from './components/Footer/Footer';
 import CombinedAccountFlow from './components/Form/Contact';
 import Banners from './components/Poster/Banner';
